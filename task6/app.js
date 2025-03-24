1efb47335200388680f557fa559dc3bbf4342c06
OsmanliFarid <osmanliferid686@gmail.com>
2025-03-24 16:13:47 +0400

    task6

---
+++ task6/app.js
@@ -0,0 +1,33 @@
+//6. Orijinal massivin elementlərindən ibarət yeni massiv yaradın.
+//Rəqəmlər massivi verilmişdir. Aşağıdakı qaydalarla dəyişdirilən 
+// orijinal massivin elementlərindən ibarət yeni massiv yaradın:
+
+//mənfi ədədlər müsbət olublar;
+//müsbət ədədlər ikiqat artırılıb.
+//Input:  [1, 5, -7, 3, -9, 4, -6, 2];
+//Output: [2, 10, 7, 6, 9, 8, 6, 4]
+
+
+/*
+const func = () =>{
+     let Input = [1, 5, -7, 3, -9, 4, -6, 2]
+     let newArry = []
+     let newArry2 = []
+     for(let i =0;i<Input.length;i++){
+        if(Input[i] < 0){
+            newArry += Input[i]
+            
+            
+        }
+        
+        
+        
+     }
+     let c = newArry.split("-")
+     
+     console.log(c);
+     newArry.shift()
+     
+     
+}
+func() */
